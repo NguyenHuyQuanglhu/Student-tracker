94ca469e07fadce9a9b2b0d6fca9618addf46a1e
Quang Nguyễn Huy <ohmaichuoi08@gmail.com>
2025-10-11 11:08:20 +0000

    xóa chữ nộp bài đó di

--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -9,7 +9,6 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { SidebarTrigger } from "@/components/ui/sidebar";
-import { ArrowUpFromLine } from "lucide-react";
 import { PlaceHolderImages } from '@/lib/placeholder-images';
 import { user } from "@/app/lib/mock-data";
 
@@ -20,10 +19,6 @@ export function DashboardHeader() {
     <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background/95 px-4 backdrop-blur supports-[backdrop-filter]:bg-background/60 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
       <SidebarTrigger className="sm:hidden" />
       <div className="flex flex-1 items-center justify-end space-x-4">
-        <Button>
-          <ArrowUpFromLine className="mr-2 h-4 w-4" />
-          Nộp bài
-        </Button>
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button variant="ghost" className="relative h-10 w-10 rounded-full">

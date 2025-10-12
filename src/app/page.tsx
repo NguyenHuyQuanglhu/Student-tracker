abce715085fd88052960c1aa6cef2d588e0e5c5d
Quang Nguyễn Huy <ohmaichuoi08@gmail.com>
2025-10-12 08:17:55 +0000

    xóa hết cái này

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { AcademicWarnings } from "@/components/dashboard/academic-warnings";
 import { ProgressOverview } from "@/components/dashboard/progress-overview";
 import { SoftSkillsDashboard } from "@/components/dashboard/soft-skills";
 import { TodoList } from "@/components/dashboard/todo-list";
-import { WelcomeBanner } from "@/components/dashboard/welcome-banner";
 import { YourCourses } from "@/components/dashboard/your-courses";
 
 export default function Home() {
@@ -14,7 +13,6 @@ export default function Home() {
       <DashboardSidebar>
         <main className="flex-1 p-4 sm:p-6">
           <div className="space-y-6">
-            <WelcomeBanner />
             <YourCourses />
             <div className="grid grid-cols-1 gap-6 lg:grid-cols-3">
               <div className="lg:col-span-2">
